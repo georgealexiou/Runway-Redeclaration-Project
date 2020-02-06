811034d914e0f4c37099744d957f4a0d64e00001
ga4g18 <george@alexiou.xyz>
2020-02-06 16:26:45 +0000

    Tested whether github actually works

--- runway-redeclaration/src/main/java/org/comp2211/group6/App.java
+++ runway-redeclaration/src/main/java/org/comp2211/group6/App.java
@@ -6,20 +6,22 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class App extends Application
-{
-    public static void main( String[] args )
-    {
-      launch(args);
-    }
+public class App extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-      String javaVersion = System.getProperty("java.version");
-      String javafxVersion = System.getProperty("javafx.version");
-      Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-      Scene scene = new Scene(new StackPane(l), 640, 480);
-      primaryStage.setScene(scene);
-      primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    String javaVersion = System.getProperty("java.version");
+    String javafxVersion = System.getProperty("javafx.version");
+    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public void sampleMethod() {
+    System.out.println("hello");
+  }
 }

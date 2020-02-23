4634fa300d94bc845f638416408a45984603af38
Jonathan Bartlett <jonathanscott2047@gmail.com>
2020-02-23 12:50:35 +0000

    Added an airport stubbed out class

---
+++ src/main/java/org/comp2211/group6/Model/Airport.java
@@ -0,0 +1,26 @@
+package org.comp2211.group6.Model;
+
+import java.util.List;
+
+public class Airport {
+
+  /**
+     Public Methods
+   */
+  public Airport(string name) {}
+
+  public void addRunway(Runway runway) {}
+  public void removeRunway(Runway runway) {}
+  public List<Runway> getRunways() { return null; }
+
+  /**
+     Private Methods
+   */
+
+  /**
+     Private Properties
+   */
+  private final string name;
+  private List<Runway> runways;
+
+}

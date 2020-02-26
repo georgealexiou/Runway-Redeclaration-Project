a361f32ce4808eeaa7bb2e98ea2ce31c3db0fad5
georgealexiou <george@alexiou.xyz>
2020-02-26 00:25:23 +0000

    Added some comments to the methods in CalculationOutput.java

--- src/main/java/org/comp2211/group6/Controller/CalculationOutput.java
+++ src/main/java/org/comp2211/group6/Controller/CalculationOutput.java
@@ -16,6 +16,16 @@ public class RedeclarationMap{
         this.map =  new HashMap<String,String>();
     }
 
+    /**
+     * Method that generates a new String representing the calculations based on the
+     * following parameters and adds it to the map
+     * @param logicalRunway A logical runway from which data is extracted
+     * @param type The type of calculation that was performed
+     * @param obstacle The obstacle in the runway/logical runway
+     * @param RESA 
+     * @param StripEnd
+     * @param CentreLine
+     */
     public void add(LogicalRunway logicalRunway, CalculationType type, Obstacle obstacle, int RESA, int StripEnd, int CentreLine){
         this.logicalRunway = logicalRunway;
         this.obstacle = obstacle;
@@ -49,6 +59,9 @@ public class RedeclarationMap{
         }
     }
 
+    /**
+     * @return Map of all calculationTypes and logicalrunways to String output
+     */
     public HashMap<String,String> getMap(){
         return map;
     }
@@ -63,7 +76,7 @@ public class RedeclarationMap{
     /**
      * Returns a String that represents the calculations performed
      * @return RLDA = Distance from Threshold - RESA - Strip End
-     */
+    */
     private String landingOver(){
         String output = "RLDA = Distance from Threshold - RESA - Strip End";
         output.concat("\n     = " + 555 + " - " + otherParameters.get("RESA") + " - " + otherParameters.get("StripEnd"));
@@ -71,6 +84,12 @@ public class RedeclarationMap{
         return output;
     }
 
+    /**
+     * Returns a string that represents the calculations performed
+     * @return RTORA = TORA - Blast Protection - Distance from Threshold - Displaced Threshold
+     *         RASDA = RTORA + STOPWAY
+     *         RTODA = RTORA + CLEARWAY
+    */
     private String landingTowards(){
         String output = "RLDA = Distance from Threshold - Strip End - Slope Calculation";
         output.concat("\n     = " + 555 + " - " + otherParameters("StripEnd") + " - (" + 55 + "*" + 50 + ")");
@@ -79,6 +98,12 @@ public class RedeclarationMap{
         return output;
     }
 
+    /**
+     * Returns a string that represents the calculations performed
+     * @return RTORA = TORA - Blast Protection - Distance from Threshold - Displaced Threshold
+     *         RASDA = RTORA + STOPWAY
+     *         RTODA = RTORA + CLEARWAY
+    */
     private String takeOffAway(){
         String output = "RTORA = TORA - Blast Protection - Distance from Threshold - Displaced Threshold";
         output.concat("\n      = " + logicalRunway.getParameters().getTORA() + " - " + 555 + " - " + 100);
@@ -91,6 +116,12 @@ public class RedeclarationMap{
         return output;
     }
 
+    /**
+     * Returns a string that represents the calculations performed
+     * @return RTORA = Distance from Threshold - Slope Calculation
+     *         RASDA = RTORA
+     *         RTODA = RTORA
+    */
     private String takeOffTowards(){
         String output = "RTORA = Distance from Threshold - Slope Calculation";
 

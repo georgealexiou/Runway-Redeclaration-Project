a757f8f8ee395f9a1b35f8140b7bd738497920fd
georgealexiou <george@alexiou.xyz>
2020-02-28 15:39:35 +0000

    Adjusted the way recalculated parameters are stored in the logical runway

--- src/main/java/org/comp2211/group6/Model/LogicalRunway.java
+++ src/main/java/org/comp2211/group6/Model/LogicalRunway.java
@@ -1,79 +1,54 @@
 package org.comp2211.group6.Model;
 
-import org.comp2211.group6.Model.RunwayParameters;
-
 public class LogicalRunway {
 
   /**
-      Create a new logical runway
+      Constructor for logical runway class
 
       @param  heading  The heading of the runway 1-36
       @param  displacedThreshold  The displaced threshold of the runway
       @param  position  The position of the runway L,R,C
       @param  params  The base runway parameters
    */
-  public LogicalRunway(int heading, double displacedThreshold,
-                       char position, RunwayParameters params) {
-    if(heading < 1 | heading > 36)
-      throw new IllegalArgumentException("Error. Invalid heading, which should be an Integer between 1 and 36.");
-    if(displacedThreshold < 0)
-      throw new IllegalArgumentException("Error. Invalid displaced threshold, which should be non-negative.");
-    if(position != 'L' && position != 'R' && position != 'C')
-		  throw new IllegalArgumentException("Error. Invalid position, which can be 'L' or 'R' or 'C'.");
-		  
+  public LogicalRunway(int heading, double displacedThreshold, char position, RunwayParameters params) {
+      if(heading < 1 | heading > 36)
+          throw new IllegalArgumentException("Invalid heading. Should be int 1-36");
+      if(displacedThreshold < 0)
+          throw new IllegalArgumentException("Displaced Threashold can't be negative");
+      if(position != 'L' && position != 'R' && position != 'C')
+		  throw new IllegalArgumentException("Invalid position. Can be 'L','R','C'");
+
 	  this.heading = heading;
 	  this.displacedThreshold = displacedThreshold;
 	  this.position = position;
 	  this.originalParameters = params;
+	  this.recalculatedParameters = new RunwayParameters();
   }
-  
-  /**
-      Public Methods
-  */
-  
+
+  /** Public Methods */
+
   /** Returns an identifier e.g. 09L */
   public String getIdentifier() {
     if(heading < 10)
       return "0" + Integer.toString(heading) + position;
-    
+
 	  return Integer.toString(heading) + position;
   }
 
   /** Returns the heading of the identifier e.g. 9, 27 etc. */
   public int getHeading(){ return heading; }
-  
+
   /** @return The original RunwayParameters Object */
   public RunwayParameters getParameters() { return originalParameters; }
-  
+
   /** @param runwayParameters: Object that filled in with recalculated parameters */
-  public void setRecalculatedParameters(RunwayParameters runwayParameters) { recalculatedParameters = runwayParameters; }
-  
-  /** set individual runway parameters */
-  public void setRecalculatedParameter(String name, double params) {
-    RunwayParameters runwayParameters = new RunwayParameters(
-        originalParameters.getTORA(), originalParameters.getTODA(),
-        originalParameters.getASDA(), originalParameters.getLDA());
-    
-    if(name == "RTORA") {
-      runwayParameters.setTORA(params);
-	  }else if(name == "RTODA") {
-		  runwayParameters.setTODA(params);
-	  }else if(name == "RASDA") {
-		  runwayParameters.setASDA(params);
-	  }else if(name == "RLDA") {
-		  runwayParameters.setLDA(params);
-	  }else {
-		  throw new IllegalArgumentException("Failed to set recalculated parameter. Invalid name of parameter.");
-	  }
-	  
-	  setRecalculatedParameters(runwayParameters);
-  }
+  public void setRecalculatedParameters(RunwayParameters recalculatedParameters) { this.recalculatedParameters = recalculatedParameters; }
 
   /** Returns the displaced threshold */
   public double getDisplacedThreshold() { return displacedThreshold; }
-  
+
   /**
-   * @return if no recalculation is performed, return null (member variable object default null) 
+   * @return if no recalculation is performed, return null (member variable object default null)
    *         otherwise return the RunwayParameters Object containing recalculated parameters
    */
   public RunwayParameters getRecalculatedParameters() { return recalculatedParameters; }
@@ -89,6 +64,6 @@ public class LogicalRunway {
   private final char position;
   private double displacedThreshold;
 
-  private final RunwayParameters originalParameters;
+  private RunwayParameters originalParameters;
   private RunwayParameters recalculatedParameters;
 }

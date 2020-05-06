10c135c9632fedf388d4480d85b15cbd4429a8a4
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2020-05-06 14:13:09 +0100

    Fixed a small issue where no theme was applied on startup

--- src/main/java/org/comp2211/group6/App.java
+++ src/main/java/org/comp2211/group6/App.java
@@ -1,10 +1,12 @@
 package org.comp2211.group6;
 
+import org.comp2211.group6.Model.ColourScheme;
 import org.comp2211.group6.view.MainView;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class App extends Application {
 
@@ -16,6 +18,7 @@ public class App extends Application {
         primaryStage.setTitle("Runway Re-declaration Tool");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
+        ColourScheme.getInstance().generateRootStyleString(Window.getWindows());
     }
 
     public static void main(String[] args) {

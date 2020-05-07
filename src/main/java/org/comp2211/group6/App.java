05acd10d989a83786c0dd1c71be0b811116a151c
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2020-05-07 09:59:15 +0100

    View Scaling fix?

--- src/main/java/org/comp2211/group6/App.java
+++ src/main/java/org/comp2211/group6/App.java
@@ -5,20 +5,24 @@ import org.comp2211.group6.view.MainView;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 public class App extends Application {
+    // Finds the root default font size for DPI scaling
+    final double rem = Math.rint(new Text("").getLayoutBounds().getHeight());
 
     /** Public Methods */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Application.setUserAgentStylesheet(STYLESHEET_MODENA);
         MainView root = new MainView();
+        root.u.setEm(rem);
         primaryStage.setTitle("Runway Re-declaration Tool");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.setMinHeight(600);
-        primaryStage.setMinWidth(800);
+        primaryStage.setScene(new Scene(root, 66.6 * rem, 50 * rem));
+        primaryStage.setMinHeight(66.6 * rem);
+        primaryStage.setMinWidth(50 * rem);
         primaryStage.show();
         ColourScheme.getInstance().generateRootStyleString(Window.getWindows());
     }

afeb1382bb53dd906ee39a116bd8b049a11cf2de
georgealexiou <george@alexiou.xyz>
2020-05-09 23:02:15 +0300

    Created a new version of airport config with a new layout

--- src/main/java/org/comp2211/group6/view/AirportConfigView.java
+++ src/main/java/org/comp2211/group6/view/AirportConfigView.java
@@ -1,23 +1,32 @@
 package org.comp2211.group6.view;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.*;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import org.comp2211.group6.Model.Airport;
-import org.comp2211.group6.Model.LogicalRunway;
-import org.comp2211.group6.Model.RunwayParameters;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import org.comp2211.group6.Model.*;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -27,23 +36,28 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.util.StringConverter;
+import org.comp2211.group6.XMLHandler;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.text.AsyncBoxView;
 
 public class AirportConfigView extends GridPane implements Initializable {
-
-    /*
-    FXML Stuff
-     */
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private GridPane baseGrid;
 
     @FXML
     private Text airportConfigText;
+
+    @FXML
+    private ButtonBar optionsBar;
+
+    @FXML
+    private Button save;
+
+    @FXML
+    private Button export;
 
     @FXML
     private GridPane nameGrid;
@@ -58,25 +72,161 @@ public class AirportConfigView extends GridPane implements Initializable {
     private TextField airportName;
 
     @FXML
-    private ButtonBar optionsBar;
+    private TextField runwayName;
 
     @FXML
-    private Button save;
+    private ChoiceBox<?> selectLogicalRunway;
 
     @FXML
-    private Button export;
+    private ChoiceBox<?> selectRunway;
 
     @FXML
-    private Button delete;
+    private TextField tora;
 
     @FXML
-    private Accordion runwayAccordion;
+    private TextField toda;
+
+    @FXML
+    private TextField asda;
+
+    @FXML
+    private TextField lda;
+
+    @FXML
+    private TextField logicalRunwayName;
+
+    @FXML
+    private TextField displacedThreshold;
+
+    @FXML
+    private Button addRunway;
+
+    @FXML
+    private Button saveRunway;
+
+    @FXML
+    private Button deleteRunway;
+
+    @FXML
+    private Button addLogicalRunway;
+
+    @FXML
+    private Button saveLogicalRunway;
+
+    @FXML
+    private Button deleteLogicalRunway;
+
+    @FXML
+    void exportClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveClicked(MouseEvent event) {
+
+    }
+
+
+    /*
+    Logical Runway Buttons
+     */
+    @FXML
+    void addLogicalRunwayClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveLogicalRunwayClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void deleteLogicalRunwayClicked(MouseEvent event) {
+
+    }
+
+    /*
+    Runway Buttons
+     */
+    @FXML
+    void addRunwayClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveRunwayClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void deleteRunwayClicked(MouseEvent event) {
+
+    }
 
 
     /*
     Other Parameters
      */
     private Airport airport;
+
+    private Runway currentRunway;
+
+    private ArrayList<LogicalRunway> logicalRunways;
+    private LogicalRunway currentLogicalRunway;
+
+    /*
+    Method used to load an airport into the view
+     */
+    public void loadAirport(Airport airport){
+        this.airport = airport;
+        airportName.setText(airport.getName());
+
+        ObservableList runwayObservable = FXCollections.observableArrayList();
+        runwayObservable.addAll(airport.getRunwayNames());
+        selectRunway.setItems(runwayObservable);
+        selectRunway.valueProperty().addListener((e, oldVal, newVal) -> {
+            setRunway((String) newVal);
+        });
+        //selectRunway.getSelectionModel().selectFirst();
+    }
+
+    /*
+    Method used when changing runways
+     */
+    public void setRunway(String runwayID){
+        currentRunway = airport.getRunwayFromName(runwayID);
+        runwayName.setText(currentRunway.getName());
+
+        ObservableList logicalRunwayObservable = FXCollections.observableArrayList();
+
+        logicalRunwayObservable.addAll(currentRunway.getLogicalRunwayNames());
+        selectLogicalRunway.setItems(logicalRunwayObservable);
+        selectLogicalRunway.valueProperty().addListener((e, oldVal, newVal) -> {
+            setLogicalRunway((String) newVal);
+        });
+        //selectLogicalRunway.getSelectionModel().selectFirst();
+        if (currentRunway.getLogicalRunways().isEmpty()){
+            logicalRunwayName.setText("");
+            displacedThreshold.setText("");
+            tora.setText("");
+            toda.setText("");
+            toda.setText("");
+            toda.setText("");
+        }
+
+    }
+
+    public void setLogicalRunway(String logicalRunwayID){
+        currentLogicalRunway = currentRunway.getLogicalRunwayFromName(logicalRunwayID);
+        logicalRunwayName.setText(currentLogicalRunway.getIdentifier());
+        displacedThreshold.setText(currentLogicalRunway.getDisplacedThreshold() + "");
+        tora.setText(currentLogicalRunway.getParameters().getTORA() + "");
+        toda.setText(currentLogicalRunway.getParameters().getTODA() + "");
+        toda.setText(currentLogicalRunway.getParameters().getASDA() + "");
+        toda.setText(currentLogicalRunway.getParameters().getLDA() + "");
+
+    }
+
 
 
     public AirportConfigView() {
@@ -85,29 +235,10 @@ public class AirportConfigView extends GridPane implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-       /*
-
-        setupBreakdownPicker(FXCollections.observableArrayList());
-        this.availableBreakdowns = new HashMap<LogicalRunway, String>();
-        this.breakdownDetails.setStyle("-fx-font-family: 'monospaced';");
-        this.currentLogicalRunway = null;
-        parameterColumn.setCellValueFactory(
-                new PropertyValueFactory<BreakdownComparison, String>("property"));
-        originalValueColumn.setCellValueFactory(
-                new PropertyValueFactory<BreakdownComparison, Double>("original"));
-        recalculatedValueColumn.setCellValueFactory(
-                new PropertyValueFactory<BreakdownComparison, Double>("recalculated"));
-        valuesTable.getColumns().get(0).prefWidthProperty()
-                .bind(valuesTable.widthProperty().multiply(0.33));
-        valuesTable.getColumns().get(1).prefWidthProperty()
-                .bind(valuesTable.widthProperty().multiply(0.33));
-        valuesTable.getColumns().get(2).prefWidthProperty()
-                .bind(valuesTable.widthProperty().multiply(0.33));
-        valuesTable.setFixedCellSize(25);
-        this.getRowConstraints().get(1).prefHeightProperty()
-                .bind(Bindings.size(valuesTable.getItems())
-                        .multiply(valuesTable.getFixedCellSize()).add(30));
-                        */
+        save.setDisable(true);
+        saveRunway.setDisable(true);
+        saveLogicalRunway.setDisable(true);
+        deleteLogicalRunway.setDisable(true);
 
     }
 
